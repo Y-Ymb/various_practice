8d1830a743e87cc00db96e0a17652bb3c058b918
Y-Ymb <yamab446@gmail.com>
2019-09-03 18:27:50 +0900

    add sloppy program

---
+++ forActions/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+  int i, j = 0, max;
+
+  if (argc > 1) {
+    max = atoi(argv[1]);
+  }
+  if (max == 0) max = 100;
+
+  for (i = 0 ; i < max ; i++) {
+    j += i * (100 - i);
+  }
+
+  printf("i : %d, j : %d\n", i, j);
+  return 1;
+} 
